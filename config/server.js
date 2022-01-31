59b96f64ccd116b611d5c5ec611c7efebc0e0a67
ed1868 <ruizeduardo21@gmail.com>
2022-01-31 17:29:30 -0500

    reverting

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'https://bamboomarketplace.herokuapp.com'),
-  port: env.int('PORT', 'https://bamboomarketplace.herokuapp.com'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 5432),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0cc210dc94052987b751e32861b0d58c'),
